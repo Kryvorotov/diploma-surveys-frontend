1fa0108f4a9dfab6915d00ffdac7e2c13585de42
Danylo <danylo.kryvorotov@binary-studio.com>
2023-05-13 12:19:12 +0300

    update date util

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,6 +1,6 @@
 export const isPast = (date) =>
   new Date(date).getTime() < new Date().getTime()
 
-export const convertToInput = (date = null) => new Date(date).toISOString().split("T")[0]
+export const convertToInput = (date) => date ? new Date(date).toISOString().split("T")[0] : new Date().toISOString().split("T")[0]
 
 export const getDate = (date) => new Date(Date.parse(date)).toDateString()
